eb33b6c4d7874c675da0cd5eecd01de26b247c0f
rimger1 <rimogery@gmail.com>
2026-01-29 14:12:44 +0100

    TUS-20 szoveg kiiras teszt

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*indul a jatek*/
-const intro = document.getElementById("intro");
+/* const intro = document.getElementById("intro");
 const logo = document.getElementById("tuske-feher");
 
 setTimeout(() => {
@@ -24,10 +24,26 @@ function animacio()
   karakter.style.left = x + "px";
   requestAnimationFrame(animacio)
 }
+ */
+// szovegiras
+
+let szoveg = "Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
+const szovegTx = document.getElementById("szoveg");
+const tovBtn = document.getElementById("gomb");
+
+function Kiir(){
+  szovegTx.innerText = szoveg;
+  console.log("semmi")
+}
 
 
 
 
+/*Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... 
++Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, 
+vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! 
++ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság.
+*/
 
 
 
