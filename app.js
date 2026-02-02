803c0f3450a4e5b56cae6e51e11deb84f0a6e04d
rimger1 <rimogery@gmail.com>
2026-02-02 12:30:18 +0100

    TUS-20 type animation

--- app.js
+++ app.js
@@ -14,21 +14,34 @@ function animacio()
 
 // window.open("page.html");
 
-let szoveg = "Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
+let szoveg = "abc...+Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
 let tortSzoveg = [];
 let currantIndex = 0;
 const szovegTx = document.getElementById("szoveg");
 const tovBtn = document.getElementById("gomb");
 
-function Kiir(){
+function sleep(ms) 
+{ 
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function Kiir(){
   tortSzoveg = szoveg.split('+');
-  szovegTx.innerText = tortSzoveg[currantIndex];
+
+  szovegTx.innerText ="";
+  for (let i=0; i < tortSzoveg[currantIndex].length; i++)
+  {
+    szovegTx.innerText += tortSzoveg[currantIndex][i];
+    await sleep(100);
+
+  }
+
+
+
   if (currantIndex < tortSzoveg.length-1)
   {
     currantIndex++;
   }
-  console.log(tortSzoveg.length);
-  console.log(currantIndex);
 }
 
 
