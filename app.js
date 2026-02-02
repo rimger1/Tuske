e4a7a3b93f75749fb904b8ddbf513c29e67090e1
rimger1 <rimogery@gmail.com>
2026-02-02 12:09:52 +0100

    TUS-20 start logo fix , szoveg kiiras

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*indul a jatek*/
-/* const intro = document.getElementById("intro");
+const intro = document.getElementById("intro");
 const logo = document.getElementById("tuske-feher");
 
 setTimeout(() => {
@@ -24,16 +24,26 @@ function animacio()
   karakter.style.left = x + "px";
   requestAnimationFrame(animacio)
 }
- */
+
 // szovegiras
 
+// window.open("page.html");
+
 let szoveg = "Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
+let tortSzoveg = [];
+let currantIndex = 0;
 const szovegTx = document.getElementById("szoveg");
 const tovBtn = document.getElementById("gomb");
 
 function Kiir(){
-  szovegTx.innerText = szoveg;
-  console.log("semmi")
+  tortSzoveg = szoveg.split('+');
+  szovegTx.innerText = tortSzoveg[currantIndex];
+  if (currantIndex < tortSzoveg.length-1)
+  {
+    currantIndex++;
+  }
+  console.log(tortSzoveg.length);
+  console.log(currantIndex);
 }
 
 
