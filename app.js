99c335b9cb0b496da1c537d734c11bff40c640e4
rimger1 <rimogery@gmail.com>
2026-02-03 08:45:00 +0100

    js fileok atnevezese

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-/*
-
-// cutscene 1
-
-const karakter = document.getElementById("karakter");
-
-function animacio()
-{
-  karakter.style.left = x + "px";
-  requestAnimationFrame(animacio)
-}
-*/
-// szovegiras
-
-// window.open("page.html");
-
-
-
-
 let szoveg = "abc...+Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
 let tortSzoveg = [];
 let currantIndex = 0;
 const szovegTx = document.getElementById("szoveg");
 const tovBtn = document.getElementById("gomb");
-
-
-
-
 
 function sleep(ms) 
 { 
@@ -52,9 +29,6 @@ async function Kiir(){
     tovBtn.disabled= false;
   }
 }
-
-
-
 
 /*Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... 
 +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, 
