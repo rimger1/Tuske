5a8c638e4e839d9673d32ef02154b8f4f6267f2c
bzalan78 <bokanyizalan@turr.hu>
2026-02-05 11:27:24 +0100

    TUS-35 JS hover szövegek előtt nyíl és item megjelenik, item eltávolítás és változtatás lehetséges

---
+++ hover.js
@@ -0,0 +1,39 @@
+function elsov() {
+    const element1 = document.getElementById("valaszto");
+    element1.style.position = "absolute"; 
+    element1.style.top = "60px";
+
+    const element = document.getElementById("hasznaltitem");
+element.style.position = "absolute"; 
+element.style.top = "29px";
+
+//element.visible = false;
+}
+function masodikv() {
+    const element1 = document.getElementById("valaszto");
+    element1.style.position = "absolute";
+    element1.style.top = "112px"; 
+
+    const element = document.getElementById("hasznaltitem");
+element.style.position = "absolute";
+element.style.top = "80px"; 
+
+//element.visible = false;
+}
+function harmadikv() {
+    const element1 = document.getElementById("valaszto");
+    element1.style.position = "absolute";
+    element1.style.top = "162px"; 
+
+    const element = document.getElementById("hasznaltitem");
+element.style.position = "absolute";
+element.style.top = "130px";
+
+//element.visible = false;
+
+}
+
+
+
+
+
