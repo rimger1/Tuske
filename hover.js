1ff50a89038a13f1b75e0783cb384fad998edc28
bzalan78 <bokanyizalan@turr.hu>
2026-02-05 13:41:06 +0100

    TUS-35 slide animáció valasztokurzorhoz

--- hover.js
+++ hover.js
@@ -1,10 +1,12 @@
 function elsov() {
     const element1 = document.getElementById("valaszto");
     element1.style.position = "absolute"; 
+    element1.style.transition = "top 0.2s ease"; 
     element1.style.top = "60px";
 
     const element = document.getElementById("hasznaltitem");
 element.style.position = "absolute"; 
+element.style.transition = "top 0.2s ease"; 
 element.style.top = "29px";
 
 //element.visible = false;
@@ -13,10 +15,12 @@ element.style.top = "29px";
 function masodikv() {
     const element1 = document.getElementById("valaszto");
     element1.style.position = "absolute";
+    element1.style.transition = "top 0.2s ease"; 
     element1.style.top = "112px"; 
 
     const element = document.getElementById("hasznaltitem");
 element.style.position = "absolute";
+element.style.transition = "top 0.2s ease"; 
 element.style.top = "80px"; 
 
 //element.visible = false;
@@ -25,11 +29,13 @@ element.style.top = "80px";
 function harmadikv() {
     const element1 = document.getElementById("valaszto");
     element1.style.position = "absolute";
-    element1.style.top = "162px"; 
+    element1.style.transition = "top 0.2s ease"; 
+    element1.style.top = "162px";
 
     const element = document.getElementById("hasznaltitem");
-element.style.position = "absolute";
-element.style.top = "130px";
+    element.style.position = "absolute";
+    element.style.transition = "top 0.2s ease"; 
+    element.style.top = "130px";
 
 //element.visible = false;
 //element.backgroundImage = "url('')";
