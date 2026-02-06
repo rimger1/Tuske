aa4115e3fb9b6485d0321b5266c2853e85ae3e7e
bzalan78 <bokanyizalan@turr.hu>
2026-02-06 13:24:31 +0100

    TUS-37 karaktermozgás kezdet(bal karakter)

--- karakterarguemovement.js
+++ karakterarguemovement.js
@@ -1,5 +1,24 @@
 function elsoani(){
+    const element1 = document.getElementById("balKarakter");
+    element1.style.position = "absolute"; 
+    element1.style.transition = "left 0.4s ease"; 
+    element1.style.transition = "top 0.2s ease"; 
+    element1.style.transition = "height 0.2s ease"; 
+    element1.style.left = "20%";
+    element1.style.height = "620px";
+    element1.style.top = "120px";
 
+
+
+
+
+
+
+
+
+
+
+
+    const element2 = document.getElementById("jobbKarakter");
+    element2.style.position = "absolute"; 
 }
-function madosikani(){}
-function harmadikani(){}
