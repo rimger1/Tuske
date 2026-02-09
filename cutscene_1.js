d3707bda1c8b54316b6db663a02736c32553dc36
rimger1 <rimogery@gmail.com>
2026-02-09 12:01:22 +0100

    TUS-34 cutscene 1 be bent van a szoveg kiiras

--- cutscene_1.js
+++ cutscene_1.js
@@ -1,8 +1,17 @@
-let szoveg = "abc...+Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
+let szoveg = "Fhuu... +Szét megy a fejem... +Mi történt az este?+Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
 let tortSzoveg = [];
 let currantIndex = 0;
-const szovegTx = document.getElementById("szoveg");
-const tovBtn = document.getElementById("gomb");
+const szovegTx = document.getElementById("karakterSzoveg");
+const tovBtn = document.getElementById("next");
+let clickCounter = 0;
+
+/*fade levetele*/
+setTimeout(fadeOff, 2500);
+
+function fadeOff(){
+  document.getElementById("fade").style.display = "none";
+}
+
 
 function sleep(ms) 
 { 
@@ -10,6 +19,8 @@ function sleep(ms)
 }
 
 async function Kiir(){
+  clickCounter++;
+  console.log(clickCounter);
   tovBtn.disabled= true;
   tortSzoveg = szoveg.split('+');
 
@@ -25,10 +36,37 @@ async function Kiir(){
   if (currantIndex < tortSzoveg.length-1)
   {
     currantIndex++;
-    console.log(currantIndex)
     tovBtn.disabled= false;
   }
+  Mozgas();
 }
+
+
+function Mozgas(){}    
+  if (clickCounter == 4){
+    const szog = document.getElementById("karakter");
+    szog.style.position = "absolute";
+    szog.style.transform = "rotate(0deg)";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... 
 +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, 
