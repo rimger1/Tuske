6f3d8f99bac67352b67c5441d6395dcdf2bb9600
bzalan78 <bokanyizalan@turr.hu>
2026-02-13 13:25:04 +0100

    TUS-39 globális változók és gomb függvények

---
+++ jatekmenet.js
@@ -0,0 +1,32 @@
+const karakternev= document.getElementById("karakterNev");
+const cutszoveg= document.getElementById("karakterSzoveg");
+const elsoValaszthato= document.getElementById("elsoValaszthato");
+const masodikValaszthato= document.getElementById("masodikValaszthato");
+const harmadikValaszthato= document.getElementById("harmadikValaszthato");
+const hatter=document.getElementById("jatektest");
+
+    elsoValaszthato.style.visibility = "hidden";
+    masodikValaszthato.style.visibility = "hidden";
+    harmadikValaszthato.style.visibility = "hidden";
+
+function elsova() {
+    
+    elsoValaszthato.style.visibility = "hidden";
+    masodikValaszthato.style.visibility = "hidden";
+    harmadikValaszthato.style.visibility = "hidden";
+
+}
+function masodikva() {
+    
+    elsoValaszthato.style.visibility = "hidden";
+    masodikValaszthato.style.visibility = "hidden";
+    harmadikValaszthato.style.visibility = "hidden";
+    
+}
+function harmadikva() {
+   
+    elsoValaszthato.style.visibility = "hidden";
+    masodikValaszthato.style.visibility = "hidden";
+    harmadikValaszthato.style.visibility = "hidden";
+    
+}
