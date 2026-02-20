6bad780ff840ab3aeb698250ba787d9a624516c5
bzalan78 <bokanyizalan@turr.hu>
2026-02-20 13:25:39 +0100

    TUS-39 kiiras megjavitasa, függvényhívások

--- jatekmenet.js
+++ jatekmenet.js
@@ -1,12 +1,6 @@
 const element1 = document.getElementById("balKarakter");
 const element2 = document.getElementById("jobbKarakter");
 
-let szoveg = "Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
-let tortSzoveg = [];
-let currantIndex = 0;
-
-
-
 
 const elsoValaszthato= document.getElementById("elsoValaszthato");
 const masodikValaszthato= document.getElementById("masodikValaszthato");
@@ -19,8 +13,46 @@ const valaszto=document.getElementById("valaszto");
 const item=document.getElementById("hasznaltitem");
 
 const hatter=document.getElementById("jatektest");
+hatter.style.backgroundImage = `url("kepek/tuskekilakoltatas.png")`;
 
 
+function balkarakterbeszel(){
+  const element1 = document.getElementById("balKarakter");
+    element1.style.position = "absolute"; 
+    element1.style.transition = "left 0.4s ease, top 0.2s ease, height 0.2s ease"; 
+    element1.style.left = "10%";
+    element1.style.height = "660px";
+    element1.style.top = "120px";
+
+    const element2 = document.getElementById("jobbKarakter");
+    element2.style.position = "absolute"; 
+    element2.style.transition = "right 0.4s ease, top 0.2s ease, height 0.2s ease"; 
+    element2.style.right = "5%";
+    element2.style.height = "510px";
+    element2.style.top = "198px";
+}
+function jobbkarkterbeszel(){
+  const element1 = document.getElementById("balKarakter");
+  element1.style.position = "absolute"; 
+  element1.style.transition = "left 0.4s ease, top 0.2s ease, height 0.2s ease";
+  element1.style.left = "5%";
+  element1.style.height = "550px";
+  element1.style.top = "180px";
+
+  const element2 = document.getElementById("jobbKarakter");
+  element2.style.position = "absolute"; 
+  element2.style.transition = "right 0.4s ease"; 
+  element2.style.position = "absolute"; 
+  element2.style.transition = "right 0.4s ease, top 0.2s ease, height 0.2s ease"; 
+  element2.style.right = "10%";
+  element2.style.height = "630px";
+  element2.style.top = "130px"; 
+}
+
+
+let szoveg = "Fhuu... +Szét megy a fejem... +Mi történt az este? +Mennyi az idő egyáltalán? +Bakker jól ellehettünk... (buli jelenet) +Hol van Tüske amúgy? Sehol se látom... +Asszem együtt jöttünk haza, mert emlékszek, hogy elhányta magát a konyhában. +Merre van ez a gyerek? Felhívom. +*Távoli csörgés* +Ez a csávó most vagy itt van, vagy itthagyta a telóját. +Na megnézem mizu. +HELLÓ?? +HALLÓ! +Ez tényleg meglépett a telója nélkül... +Ez a pénztárcája? Nem hiszem el, egyszer a fejét fogja elhagyni! +ITT VAGY?? HELLÓ? +Szerintem hazament hajnalban és itthagyott mindent a részeg fejével... +Mindegy elmegyek hozzá, megkérdem hogy milyen a másnaposság."
+let tortSzoveg = [];
+let currantIndex = 0;
 
 
 function sleep(ms) 
@@ -35,6 +67,7 @@ async function Kiir(){
   cutszoveg.innerHTML ="";
   for (let i=0; i < tortSzoveg[currantIndex].length; i++)
   {
+    console.log (tortSzoveg[currantIndex][i]);
     cutszoveg.innerHTML += tortSzoveg[currantIndex][i];
     await sleep(Math.floor(Math.random() * (100 - 20 + 1) + 20));
   }
@@ -43,14 +76,22 @@ async function Kiir(){
     currantIndex++;
     tovabbgomb.disabled= false;
   }
+  
+}
+function tovabbg(){
+  
+  Kiir();
+  storytelling();
+  jobbkarkterbeszel();
+
 }
 
 
 
+
+
+
 valasztas();
-
-function tgombnyom(){ Kiir();}
-
 
 
 
@@ -64,6 +105,7 @@ function storytelling()
         item.style.visibility="hidden";
         valaszto.style.visibility = "hidden";
         tovabbgomb.style.visibility = "visible";
+        
        
 
         
@@ -86,28 +128,28 @@ function valasztas()
 
 function elsova() {
     
-    elsoValaszthato.style.visibility = "hidden";
-    masodikValaszthato.style.visibility = "hidden";
-    harmadikValaszthato.style.visibility = "hidden";
+    
     storytelling();
+    Kiir();
+    jobbkarkterbeszel();
     
 
 }
 function masodikva() {
     
-    elsoValaszthato.style.visibility = "hidden";
-    masodikValaszthato.style.visibility = "hidden";
-    harmadikValaszthato.style.visibility = "hidden";
+    
     storytelling();
+    Kiir();
+    jobbkarkterbeszel();
     
     
 }
 function harmadikva() {
    
-    elsoValaszthato.style.visibility = "hidden";
-    masodikValaszthato.style.visibility = "hidden";
-    harmadikValaszthato.style.visibility = "hidden";
+   
     storytelling();
+    Kiir();
+    jobbkarkterbeszel();
     
     
 }
